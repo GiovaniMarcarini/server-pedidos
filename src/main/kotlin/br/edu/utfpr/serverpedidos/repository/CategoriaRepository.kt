3c57c2fd6af535e9835037df4a5aa48309cae8e6
GiovaniMarcarini <inf_giovani@hotmail.com>
2024-05-25 11:15:21 -0300

    Paginação

--- src/main/kotlin/br/edu/utfpr/serverpedidos/repository/CategoriaRepository.kt
+++ src/main/kotlin/br/edu/utfpr/serverpedidos/repository/CategoriaRepository.kt
@@ -3,8 +3,8 @@ package br.edu.utfpr.serverpedidos.repository
 import br.edu.utfpr.serverpedidos.entity.Categoria
 import org.springframework.data.domain.Page
 import org.springframework.data.jpa.repository.JpaRepository
-import java.awt.print.Pageable
 
 interface CategoriaRepository : JpaRepository<Categoria, Int> {
     fun findByNomeContainsIgnoreCase(nome: String): List<Categoria>
+    fun findByNomeContainsIgnoreCase(nome: String, pageable: org.springframework.data.domain.Pageable): Page<Categoria>
 }
